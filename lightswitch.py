81a598cd29ed232e96edb678cd57c857c5e3ff50
Lara Mol <89733492+LaraMol@users.noreply.github.com>
2022-02-02 09:01:03 +0100

    Add files via upload

--- lightswitch.py
+++ lightswitch.py
@@ -1,13 +1,13 @@
 import tkinter as tk
 window = tk.Tk()
 
-button = tk.Button(text='...', bg="white", fg="black")
+def toggle():
+    if window['background'] == 'white':
+        window['background'] = 'pink'
+    else:
+        window['background'] = 'white'
+
+button = tk.Button(text='Knop', bg="white", fg="black", command=toggle)
 button.pack(pady = 20, padx = 20)
 
-# schijf hier tussen je code
-
-
-
-# schijf hier tussen je code
-
-window.mainloop()
+window.mainloop() 
